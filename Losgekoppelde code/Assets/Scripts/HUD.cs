91c9e297d2832ed16a76221a98e4dad041277a5f
LCYMike <33358967+LCYMike@users.noreply.github.com>
2019-01-14 09:22:01 +0100

    add's HUD and Inventory Script

---
+++ Losgekoppelde code/Assets/Scripts/HUD.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour {
+
+    private List<string> inv;
+
+    private void Start()
+    {
+        inv = new List<string>();
+    }
+
+    void Update()
+    {
+
+        Debug.Log(inv);
+
+    }
+}
---
+++ Losgekoppelde code/Assets/Scripts/Inventory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    private List<string> inv;
+
+    private void Start()
+    {
+        inv = new List<string>();
+    }
+
+}
